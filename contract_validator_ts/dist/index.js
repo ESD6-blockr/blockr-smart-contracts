ec3f923dd8eca48dfe4555c00fe317bf267c992d
LorensoD <lorenso.dagostino@gmail.com>
2019-05-08 10:46:45 +0200

    fixes typo

--- contract_validator_ts/dist/index.js
+++ contract_validator_ts/dist/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var world = '🗺️';
 function hello(word) {
     if (word === void 0) { word = world; }
-    return "Hello " + world + "! ";
+    return "Hello " + word + "! ";
 }
 exports.hello = hello;
 //# sourceMappingURL=index.js.map
