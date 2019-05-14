a1257f15b5b189c80433c83b30b97726cf8bfa37
LorensoD <lorenso.dagostino@gmail.com>
2019-05-14 15:08:20 +0200

    executor improvemetns

--- contract_validator_ts/dist/src/executor.js
+++ contract_validator_ts/dist/src/executor.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Executor {
+    constructor() {
+    }
+    executeContract(contract, toExecute, properties) {
+        if (toExecute == null) {
+            // The contract needs to be constructed
+        }
+        else {
+            // The specified method should be called
+        }
+    }
 }
 exports.Executor = Executor;
 //# sourceMappingURL=executor.js.map
--- contract_validator_ts/src/executor.ts
+++ contract_validator_ts/src/executor.ts
@@ -1,3 +1,16 @@
+import { Json } from "serialazy";
+
 export class Executor {
+    constructor() {
+    }
     
+    executeContract(contract: Object, toExecute: string, properties: Json) {
+        if(toExecute == null) {
+            // The contract needs to be constructed
+            
+        } else {
+            // The specified method should be called
+
+        }
+    }
 }
