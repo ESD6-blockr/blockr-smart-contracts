fe57249d1bfbe1915b2e92267023a1a38b0f664c
Joel Castillo Guerra <joelcastillog@gmail.com>
2019-05-15 10:10:40 +0200

    Adds serializer class

--- contract_validator_ts/src/serializer.ts
+++ contract_validator_ts/src/serializer.ts
@@ -4,6 +4,11 @@ export class Serialize {
      } 
 
      deserialize(data: Object) {
+
+          
+          let request = JSON.parse('{ "functionName":"constructor", "functionParameters" : [ "ipfs", "owner address", [ "address1", "address2", "address3" ] ], "contract":"CONTRACT HERE"}')
+
+          console.log(request.functionName);
         
      }
 }
