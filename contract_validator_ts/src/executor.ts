a02eac3d242c6193ba7857f55af3282dc685bef3
Unknown <wtstas@gmail.com>
2019-05-21 10:41:26 +0200

    added executor

--- contract_validator_ts/src/executor.ts
+++ contract_validator_ts/src/executor.ts
@@ -1,12 +1,28 @@
 export class Executor {
-    constructor() {
+    rebuildContract(contractJson: any) {
+        let contractTemplate = eval('(' + contractJson.classTemplate.contract + ')');
+        let constructorParams = contractJson["constructor"];
+        let args: any[] = [];
+
+        for(let param in constructorParams) {
+            if(constructorParams.hasOwnProperty(param)) {
+                args.push(constructorParams[param]);
+            }
+        }
+        return new contractTemplate(...args);
     }
-    
-    executeContract(contract: any) {
-        
-        // console.log(contract)
 
-        // console.log(eval(contract.classTemplate.contract));
+    executeFunction(classInstance: any, functionJson: any) {
+        let functionName = functionJson["functionName"];
+        let functionParams = functionJson["functionParameters"];
 
+        let args: any[] = [];
+
+        for(let param in functionParams) {
+            if(functionParams.hasOwnProperty(param)) {
+                args.push(functionParams[param]);
+            }
+        }
+        classInstance[functionName](...args);
     }
 }
