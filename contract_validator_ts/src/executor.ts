87ade4b8a436fbf88eaddb8f60754a67d8f87363
Unknown <wtstas@gmail.com>
2019-05-23 10:42:35 +0200

    added a parameter check

--- contract_validator_ts/src/executor.ts
+++ contract_validator_ts/src/executor.ts
@@ -1,4 +1,4 @@
-import {getControlFlowEnd} from "tsutils";
+import {IPeer} from '@blockr/blockr-p2p-lib'
 
 export class Executor {
     /**
@@ -7,18 +7,43 @@ export class Executor {
      * @returns a instance of the contract
      */
     private static rebuildContract(contractJson: any) {
-
         let contractTemplate = this.getContractTemplate(contractJson);
         let constructorParams = contractJson["constructor"];
         let args: any[] = [];
 
+        //if the constructor in the Json is incorrect constructorParams will be the default constructor
+        //if this is the case constructorParams will be a function and null will be returned to show that a error occurred
+        if(constructorParams instanceof Function) {
+            return null;
+        }
+
         for (let param in constructorParams) {
             if (constructorParams.hasOwnProperty(param)) {
+                if (!this.checkParameter(contractTemplate.prototype, "constructor", param)) {
+                    return null;
+                }
                 args.push(constructorParams[param]);
             }
         }
         return new contractTemplate(...args);
     }
+
+    /**
+     * checks if a parameter is indeed part of the given function
+     * @param contractPrototype, a prototype of the contract
+     * @param functionName, the name of the function to execute
+     * @param parameterName, the parameter to check
+     * @returns true if the parameter is indeed included, else false is returned
+     */
+    private static checkParameter(contractPrototype: any, functionName: string, parameterName: string) {
+        let functions = Object.getOwnPropertyDescriptors(contractPrototype);
+        if (functions[functionName]) {
+            let params = this.getParamNames(functions[functionName].value);
+            return params.includes(parameterName);
+        }
+        return false;
+    }
+
 
     /**
      * creates a template of a contract from Json, can be improved by validating the evaluated contracts
@@ -43,6 +68,9 @@ export class Executor {
 
         for (let param in functionParams) {
             if (functionParams.hasOwnProperty(param)) {
+                if (!this.checkParameter(Object.getPrototypeOf(classInstance), functionName, param)) {
+                    return null;
+                }
                 args.push(functionParams[param]);
             }
         }
@@ -56,14 +84,16 @@ export class Executor {
      */
     public static executeContract(data: any) {
         let contract: object = this.rebuildContract(data);
-
         let functions = data["function"];
+        let result: any;
 
-        if(functions) {
-            this.executeFunction(contract, functions);
+        if (functions && contract !== null) {
+            result = this.executeFunction(contract, functions);
+            if (result !== null) {
+                return this.returnJson(contract, result, data.classTemplate.contract);
+            }
         }
-
-        return this.returnJson(contract, data.classTemplate.contract);
+        return null;
     }
 
     /**
@@ -82,7 +112,7 @@ export class Executor {
             if (functions.hasOwnProperty(val)) {
                 let func = functions[val].value;
                 let paras = this.getParamNames(func);
-                functionArray.functions.push({ functionName: val, parameters: paras });
+                functionArray.functions.push({functionName: val, parameters: paras});
             }
         }
         return JSON.stringify(functionArray);
@@ -103,14 +133,16 @@ export class Executor {
     }
 
     /**
-     * gets the parameters from the given function
+     * builds Json to return after a function is completed
      * @param contract, the contract object that has been executed
+     * @param result, the result of the executed function
      * @param template, the template of this contract
      * @returns json that needs to be saved on the blockchain
      */
-    private static returnJson(contract: object, template: string) {
+    private static returnJson(contract: object, result: any, template: string) {
         return JSON.stringify({
             "constructor": JSON.stringify(contract),
+            "result": result,
             "classTemplate": {
                 "contract": template
             }
