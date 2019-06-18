a66632ebf715276006fd4a5339d03bb3af92fc48
Unknown <wtstas@gmail.com>
2019-06-18 10:43:33 +0200

    added getting contracts from api

--- contract_validator_ts/src/api-calls.ts
+++ contract_validator_ts/src/api-calls.ts
@@ -1,9 +1,26 @@
 import * as request from "request-promise-native";
 
-export async function getContract(): Promise<any> {
+export async function getTransactions(): Promise<any> {
     try {
         const baseUrl = 'http://localhost:3000';
         const queryString = '/ptsmock/transaction';
+        let options = {
+            uri: baseUrl + queryString,
+        };
+
+        const result = await request.get(options);
+        return new Promise<any>(resolve => {
+            resolve(result)
+        });
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export async function getContract(id: string): Promise<any> {
+    try {
+        const baseUrl = 'http://localhost:3000';
+        const queryString = '/ptsmock/contract/' + id;
         let options = {
             uri: baseUrl + queryString,
         };
--- contract_validator_ts/src/index.ts
+++ contract_validator_ts/src/index.ts
@@ -2,21 +2,22 @@ import "reflect-metadata";
 import {IPeer, Message, Peer, PeerType} from "@blockr/blockr-p2p-lib";
 import {RESPONSE_TYPE} from "@blockr/blockr-p2p-lib/dist/interfaces/peer";
 import {Executor} from "./executor";
-import {data, data2, pairs} from "./contractString";
-import {getContract} from "./api-calls";
-import {hasOwnProperty} from "tslint/lib/utils";
+import {getContract, getTransactions} from "./api-calls";
 
 let peer: IPeer;
 
 async function startPeer() {
     peer = new Peer(PeerType.SMART_CONTRACT_ENGINE);
 
-    await peer.init(["145.93.164.143"], "8082");
+    await peer.init(["145.93.108.52"], "8082");
     await peer.registerReceiveHandlerForMessageType("wallet_getFunctions", async (message: Message, senderGuid: string, response: RESPONSE_TYPE) => {
         if (message && senderGuid) {
-            let contract = pairs[message.body];
-            message.body = Executor.getContractFunctions(contract);
-            response(message);
+            getContract(message.body).then(res => {
+                let transaction = JSON.parse(res);
+                let contractJson = JSON.parse(transaction["transactionHeader"]["smartContractData"]);
+                message.body = Executor.getContractFunctions(contractJson);
+                response(message);
+            });
         }
     });
 
@@ -30,13 +31,31 @@ async function startPeer() {
 
     await peer.registerReceiveHandlerForMessageType("wallet_getAllHashes", async (message: Message, senderGuid: string, response: RESPONSE_TYPE) => {
         if (message && senderGuid) {
-            getContract().then(res => {
+            getTransactions().then(res => {
                 let transactions = JSON.parse(res);
                 let hashes: any[] = [];
-                for(let transaction of transactions) {
-                    hashes.push(Executor.rebuildContract(JSON.parse(transaction["transactionHeader"]["smartContractData"]),false)["ipfsHash"])
+                for (let transaction of transactions) {
+                    hashes.push(Executor.rebuildContract(JSON.parse(transaction["transactionHeader"]["smartContractData"]), false)["ipfsHash"])
                 }
                 message.body = JSON.stringify(hashes);
+                response(message);
+            });
+        }
+    });
+
+    await peer.registerReceiveHandlerForMessageType("wallet_getTransactions", async (message: Message, senderGuid: string, response: RESPONSE_TYPE) => {
+        if (message && senderGuid) {
+            getTransactions().then(res => {
+                message.body = res;
+                response(message);
+            });
+        }
+    });
+
+    await peer.registerReceiveHandlerForMessageType("wallet_getContract", async (message: Message, senderGuid: string, response: RESPONSE_TYPE) => {
+        if (message && senderGuid) {
+            getContract(message.body).then(res => {
+                message.body = res;
                 response(message);
             });
         }
